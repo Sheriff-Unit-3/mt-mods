bd87f559c5777c3c8bb1fa2f4fc3486d41b14f8a
Sheriff-Unit-3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2026-07-30 22:51:25 -0500

    test part 2

--- test.lua
+++ test.lua
@@ -3,5 +3,6 @@ badly_formatted.test_function = function(string) if string then return string en
 badly_formatted.test_table = {data = "string", data2 = 1, data3 = {data1 = "string", data2 = 1, data3 = function(table) return table.tostring() end}}
 for _, data in pairs(badly_formatted.test_table) do
 	badly_formatted.test_function(data)
-	badly_formatted.test_table.data3.data3(data)
+	local data2 = badly_formatted.test_table.data3.data3(data)
+	print(data2)
 end
